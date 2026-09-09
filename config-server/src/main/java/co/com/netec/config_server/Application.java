4ea4221f9ff0c08627ca2c263c00731a0c187faa
Wilman Ortiz Navarro <willman.ortiz@gmail.com>
2026-09-09 00:58:11 -0500

    feat: Agrega soporte de spring cloud(config, eureka) y aplica ajustes a productos, pendiente users y agregar gateway al circuito

--- config-server/src/main/java/co/com/netec/config_server/Application.java
+++ config-server/src/main/java/co/com/netec/config_server/Application.java
@@ -1,11 +1,11 @@
-package co.com.netec.products;
+package co.com.netec.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-@EnableDiscoveryClient 
 public class Application {
 
 	public static void main(String[] args) {
--- eureka-server/src/main/java/co/com/netec/eureka_server/Application.java
+++ eureka-server/src/main/java/co/com/netec/eureka_server/Application.java
@@ -1,11 +1,11 @@
-package co.com.netec.products;
+package co.com.netec.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient 
+@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
