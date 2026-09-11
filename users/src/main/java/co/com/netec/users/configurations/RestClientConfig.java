4781c5b8a7366e40648d5141a044345201c297f9
Wilman Ortiz Navarro <willman.ortiz@gmail.com>
2026-09-10 22:48:37 -0500

    fix: ajusta configuraciones del microservicio de usuarios y ajustes en el load balancer para que desde usuarios se puedan consultar los productos

--- users/src/main/java/co/com/netec/users/configurations/RestClientConfig.java
+++ users/src/main/java/co/com/netec/users/configurations/RestClientConfig.java
@@ -3,20 +3,51 @@ package co.com.netec.users.configurations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.restclient.autoconfigure.RestClientBuilderConfigurer;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
     private static final Logger log = LoggerFactory.getLogger(RestClientConfig.class);
 
+    // @Bean
+    // @LoadBalanced
+    // public RestClient.Builder loadBalancedBuilder() {
+    // return RestClient.builder();
+    // }
+    //
+    // @Bean
+    // public RestClient productRestClient(RestClient.Builder loadBalancedBuilder,
+    // @Value("${api.products.url}") String baseUrl) {
+    // log.info("[url:{}][message: url to connect api products via Eureka
+    // LoadBalancer...]", baseUrl);
+    // return loadBalancedBuilder
+    // .baseUrl(baseUrl)
+    // .build();
+    // }
+
     @Bean
-    public RestClient productRestClient(RestClient.Builder client,
+    @Primary
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public RestClient.Builder restClientBuilder(RestClientBuilderConfigurer configurer) {
+        return configurer.configure(RestClient.builder());
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder loadBalancedRestClientBuilder(RestClientBuilderConfigurer configurer) {
+        return configurer.configure(RestClient.builder());
+    }
+
+    @Bean
+    public RestClient serviceARestClient(@LoadBalanced RestClient.Builder builder,
             @Value("${api.products.url}") String baseUrl) {
-        log.info("[url:{}][message: url to connect api products...]", baseUrl);
-        return client
-                .baseUrl(baseUrl)
-                .build();
+        return builder.baseUrl(baseUrl).build();
     }
 }
